f42a6798eb7fa08aee7ad5ed67a11a801fa8050c
Shiho Mukaida <canarygrass02@gmail.com>
2016-02-14 18:41:13 +0900

    編集まで完成（だけどあんまりよく分かっていない）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,34 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :price,
+      :email, :seller_id, :image_url
+      )
+  end
+
 end
