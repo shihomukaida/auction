0938d3c440d33ce60286e4a9a51e31177eb8a332
Shiho Mukaida <canarygrass02@gmail.com>
2016-02-14 19:31:48 +0900

    削除ツールを学びました

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   get '/items/new' => 'items#new'
   get '/items/:id' => 'items#show', as: :item
   patch '/items/:id' => 'items#update'
+  delete '/items/:id' => 'items#destroy'
   get 'items/:id/edit' => 'items#edit'
   get '/items' => 'items#index'
   post '/items' => 'items#create'
+
+  root 'items#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
